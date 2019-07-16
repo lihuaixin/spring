6e2e9c500ee213bf9544e900bcd0f304426c88e0
lihuaixin <lihuaixin@edspay.com>
2019-07-16 14:48:34 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step2/package-info.java
+++ ioc/src/main/java/com/lhx/spring/step2/package-info.java
@@ -2,7 +2,7 @@
  * 介绍注解使用
  * @see @Value
  * @see @PropertySource("classpath:step2.properties")
- * @see @Autowired 自动注入bean支持不存在bean时注入不会报错
+ * @see @Autowired 自动注入bean支持不存在bean时注入不会报错，声名的bean名称找不到会按类型找
  * @see @Resource  自动注入bean JSR250标准注解
  * @see @Qualifier 当多个beanid 时可以指定注入beanId
  * @see @Primary 当多个相同bean指定优先使用

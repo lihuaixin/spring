ba372986305ceb54ccceda88615c4ee756cba93b
lihuaixin <lihuaixin@edspay.com>
2019-07-16 14:44:37 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step2/package-info.java
+++ ioc/src/main/java/com/lhx/spring/step2/package-info.java
@@ -2,6 +2,10 @@
  * 介绍注解使用
  * @see @Value
  * @see @PropertySource("classpath:step2.properties")
- * Created by lihuaixin on 2019/7/16 14:09
+ * @see @Autowired 自动注入bean支持不存在bean时注入不会报错
+ * @see @Resource  自动注入bean JSR250标准注解
+ * @see @Qualifier 当多个beanid 时可以指定注入beanId
+ * @see @Primary 当多个相同bean指定优先使用
+ *  Created by lihuaixin on 2019/7/16 14:09
  */
 package com.lhx.spring.step2;
---
+++ ioc/src/main/java/com/lhx/spring/step3/config/RootConfig.java
@@ -0,0 +1,13 @@
+package com.lhx.spring.step3.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by lihuaixin on 2019/7/16 14:36
+ */
+@Configuration
+@ComponentScan(value = {"com.lhx.spring.step3.bean"})
+public class RootConfig {
+
+}

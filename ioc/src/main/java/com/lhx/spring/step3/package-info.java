b8e7c18c95307a59e3ef54224d323f32fb6f17e4
lihuaixin <lihuaixin@edspay.com>
2019-07-16 15:29:40 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step3/package-info.java
+++ ioc/src/main/java/com/lhx/spring/step3/package-info.java
@@ -1,5 +1,7 @@
 /**
- *
+ * Aware 讲解 把spring 底层的组建租入到自定义的bean中
+ * 如：ApplicationContextAware 是利用ApplicationContextAwareProcessor来进行处理，
+ * 其他的xxxAware也类似，都有相关的processor来处理
  * Created by lihuaixin on 2019/7/16 14:06
  */
 package com.lhx.spring.step3;

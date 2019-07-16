8e5e1590a84e1105c6a5c3ec5352b2ae90e43c4d
lihuaixin <lihuaixin@edspay.com>
2019-07-16 14:35:57 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step2/package-info.java
+++ ioc/src/main/java/com/lhx/spring/step2/package-info.java
@@ -1,5 +1,7 @@
 /**
- * 介绍@Value 注解使用
+ * 介绍注解使用
+ * @see @Value
+ * @see @PropertySource("classpath:step2.properties")
  * Created by lihuaixin on 2019/7/16 14:09
  */
 package com.lhx.spring.step2;

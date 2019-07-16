1a9bc6bc275138250e5d4355059ae8363f371719
lihuaixin <lihuaixin@edspay.com>
2019-07-16 17:39:40 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step4/package-info.java
+++ ioc/src/main/java/com/lhx/spring/step4/package-info.java
@@ -1,12 +1,14 @@
 /**
  * AOP 讲解
  *
+ * AnnotationAwareAspectJAutoProxyCreator 贯穿了aop的功能
+ * org.springframework.aop.config.internalAutoProxyCreator
+ * Created by lihuaixin on 2019/7/16 14:06
  * @see @Before 运行之前 前置通知
  * @see @After  运行之后 后置通知
  * @see @AfterReturning 返回之前 后置返回通知
  * @see @AfterThrowing 异常发生后 异常通知
  * @see @Around 运行执行前 before之前 环绕通知
  * @see @Pointcut("execution(public int com.lhx.spring.step4.aop.Calculator.*(..))") 切点
- * Created by lihuaixin on 2019/7/16 14:06
  */
 package com.lhx.spring.step4;

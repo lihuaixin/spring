03446958fb8796501d368cf2dd43fe9fbd671d1e
lihuaixin <lihuaixin@edspay.com>
2019-07-17 17:23:50 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step1/package-info.java
+++ ioc/src/main/java/com/lhx/spring/step1/package-info.java
@@ -7,7 +7,8 @@
  * AbstractAutowireCapableBeanFactory.createBean()--->AbstractAutowireCapableBeanFactory.doCreateBean()--->
  * AbstractAutowireCapableBeanFactory.createBeanInstance() beanWrapper对象被创建--->
  * AbstractAutowireCapableBeanFactory.populateBean()bean属性赋值--->AbstractAutowireCapableBeanFactory.initializeBean()方法初始化并
- * --->processors 处理器 执行initMethod方法；
+ * --->AbstractAutowireCapableBeanFactory.applyBeanPostProcessorsBeforeInitialization() 前置处理 执行AbstractAutowireCapableBeanFactory.initMethod方法
+ * --->AbstractAutowireCapableBeanFactory.applyBeanPostProcessorsAfterInitialization() 后置处理
  * Created by lihuaixin on 2019/7/16 14:10
  */
 package com.lhx.spring.step1;

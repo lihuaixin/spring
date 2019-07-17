2398664ae2c4ac3206238a0a1124daa89ed9d157
lihuaixin <lihuaixin@edspay.com>
2019-07-17 17:21:39 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step4/package-info.java
+++ ioc/src/main/java/com/lhx/spring/step4/package-info.java
@@ -1,9 +1,22 @@
 /**
  * AOP 讲解
- *
- * AnnotationAwareAspectJAutoProxyCreator 贯穿了aop的功能
- * org.springframework.aop.config.internalAutoProxyCreator
+ * <p>1：{@link @EnableAspectJAutoProxy} @import 了AspectJAutoProxyRegistrar类， 此类是一个ImportBeanDefinitionRegistrar注册器，注册了一个
+ * beanname 为 org.springframework.aop.config.internalAutoProxyCreator=AnnotationAwareAspectJAutoProxyCreator RootBeanDefinition
+ * <p>2：IOC容器启动时调用registerBeanPostProcessors 注册系统的所有BeanPostProcessors,在注册bean时从beanFactory中去获取bean的实例，
+ * 如果不存在则创建bean.
+ * AnnotationAwareAspectJAutoProxyCreator 的父类AbstractAutoProxyCreator 实现了 SmartInstantiationAwareBeanPostProcessor bean处理器
+ * <p>3:当创建{@link com.lhx.spring.step4.aop.Calculator } 单实例bean流程如下：
+ * finishBeanFactoryInitialization() 创建单实例bean
+ * AbstractBeanFactory.preInstantiateSingletons()--AbstractBeanFactory.getBean()-->AbstractBeanFactory.doCreateBean()--->
+ * AbstractAutowireCapableBeanFactory.createBean()--->AbstractAutowireCapableBeanFactory.doCreateBean()--->
+ * AbstractAutowireCapableBeanFactory.createBeanInstance() beanWrapper对象被创建--->
+ * AbstractAutowireCapableBeanFactory.populateBean()bean属性赋值--->AbstractAutowireCapableBeanFactory.initializeBean()方法初始化并
+ * --->applyBeanPostProcessorsBeforeInitialization() 前置处理 执行initMethod方法--->applyBeanPostProcessorsAfterInitialization() 后置处理
+ * <p>4：由于Calculator 的bean被AOP 代理了，所以代理的代码主要在applyBeanPostProcessorsAfterInitialization()中，
+ * 该方法实际是从beanFactory中获取所有的BeanPostProcessors并执行后置处理方法，AnnotationAwareAspectJAutoProxyCreator父类AbstractAutoProxyCreator.postProcessAfterInitialization()
+ * 方法，该方法中调用wrapIfNecessary()去获取CGLIB代理的bean
  * Created by lihuaixin on 2019/7/16 14:06
+ *
  * @see @Before 运行之前 前置通知
  * @see @After  运行之后 后置通知
  * @see @AfterReturning 返回之前 后置返回通知

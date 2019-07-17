243d7539274426c82ad011f016c97384531f0521
lihuaixin <lihuaixin@edspay.com>
2019-07-17 15:15:17 +0800

    ..

--- ioc/src/main/java/com/lhx/spring/step4/aop/CalculatorAsepects.java
+++ ioc/src/main/java/com/lhx/spring/step4/aop/CalculatorAsepects.java
@@ -3,13 +3,11 @@ package com.lhx.spring.step4.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.stereotype.Component;
 
 /**
  * 切面类
  * Created by lihuaixin on 2019/7/16 15:34
  */
-@Component
 @Aspect
 public class CalculatorAsepects {
     @Pointcut("execution(public int com.lhx.spring.step4.aop.Calculator.*(..))")
--- ioc/src/main/java/com/lhx/spring/step4/config/RootConfig.java
+++ ioc/src/main/java/com/lhx/spring/step4/config/RootConfig.java
@@ -1,5 +1,8 @@
 package com.lhx.spring.step4.config;
 
+import com.lhx.spring.step4.aop.Calculator;
+import com.lhx.spring.step4.aop.CalculatorAsepects;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,5 +14,13 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan(value = {"com.lhx.spring.step4"})
 @EnableAspectJAutoProxy
 public class RootConfig {
+    @Bean
+    public Calculator calculator() {
+        return new Calculator();
+    }
 
+    @Bean
+    public CalculatorAsepects calculatorAsepects() {
+        return new CalculatorAsepects();
+    }
 }

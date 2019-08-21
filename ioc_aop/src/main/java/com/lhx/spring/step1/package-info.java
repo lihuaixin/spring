7280beeb37c1c94e584de034b3c0e6e957ffc915
lihuaixin <lihuaixin@edspay.com>
2019-08-21 16:35:23 +0800

    ..

--- ioc_aop/src/main/java/com/lhx/spring/step1/package-info.java
+++ ioc_aop/src/main/java/com/lhx/spring/step1/package-info.java
@@ -1,4 +1,8 @@
 /**
+ * BeanPostProcessor  Bean后置处理器，它是Spring中定义的接口，在Spring容器的创建过程中（具体为Bean初始化前后）会回调BeanPostProcessor中定义的两个方法。在bean创建时期调用
+ * ImportSelector  spring中导入外部配置的核心接口，在SpringBoot的自动化配置和@EnableXXX(功能性注解)都有它的存在,BeanFactoryPostProcessor接口的子接口BeanDefinitionRegistryPostProcessor来实现的。
+ * ImportBeanDefinitionRegistrar
+ *
  * bean 对象的注册到容器,容器创建单实例bean
  * 1.容器启动加载配置类
  * 2.调用容器AbstractApplicationContext.refresh()
